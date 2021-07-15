6902bb7467e5d3cc727f13e8e833735a3731e1b5
Gabriel Enrique <gabrielenrique.abel@gmail.com>
2021-07-16 01:53:32 +0700

    Create html skeleton for dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,15 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Dashboard home")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("authentication:login"))
+
+    context = {}
+
+    context["metadata_fakultas"] = None
+    context["competitions"] = [1,2,3,]
+
+    return render(request, "dashboard/dashboard.html", context)
