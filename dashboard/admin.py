759604353d5585dc1d5a726c8a9a6cfab114fc20
Gabriel Enrique <gabrielenrique.abel@gmail.com>
2021-07-16 03:19:48 +0700

    Modify admin panel for metadata table

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,4 +2,9 @@ from dashboard.models import MetadataFakultas
 from django.contrib import admin
 
 # Register your models here.
-admin.site.register(MetadataFakultas)
+class MetadataFakultasPanelConfig(admin.ModelAdmin):
+    search_fields = ("username_fakultas__username", "nama_fakultas", "singkatan_fakultas")
+    ordering = ("nama_fakultas",)
+    list_display = ("nama_fakultas", "username_fakultas")
+
+admin.site.register(MetadataFakultas, MetadataFakultasPanelConfig)
--- dashboard/models.py
+++ dashboard/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class MetadataFakultas(models.Model):
-    fakultas = models.OneToOneField(User, on_delete=models.CASCADE, related_name="metadata")
+    username_fakultas = models.OneToOneField(User, on_delete=models.CASCADE, related_name="metadata")
     nama_fakultas = models.CharField(max_length=50)
     singkatan_fakultas = models.CharField(max_length=20)
     makara_image_code = models.CharField(max_length=20)
