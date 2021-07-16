8cc1d2d5fe8094257d2dce2a086eed1dff13f563
Kenta Bisma <muhammad.kenta96@gmail.com>
2021-07-16 15:36:11 +0700

    Try to add base.html and static files (Thanks Sena for the navbar)

--- mtqui/settings.py
+++ mtqui/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage',
     'gallery',
     'seminar'
 ]
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'mtqui.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mtqui/urls.py
+++ mtqui/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('homepage.urls')),
     path('gallery/', include('gallery.urls')),
     path('seminar/', include('seminar.urls'))
 ]
