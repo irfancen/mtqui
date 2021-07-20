ec0673528204706fe4050cb9e305112586ed6d5c
Gabriel Enrique <gabrielenrique.abel@gmail.com>
2021-07-20 21:54:48 +0700

    Implement login_required decorator

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
 
 # Create your views here.
+@login_required
 def home(request):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("authentication:login"))
-
     if request.user.is_superuser:
         return HttpResponseRedirect("/admin/")
 
--- mtqui/settings.py
+++ mtqui/settings.py
@@ -145,3 +145,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = "authentication:login"
