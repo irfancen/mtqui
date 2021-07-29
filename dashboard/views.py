1fdfb42973f4ecca0660187f56fcef9534dc9335
Gabriel Enrique <gabrielenrique.abel@gmail.com>
2021-07-30 03:30:51 +0700

    First implementation of enrollment form

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,12 @@
+from django.utils import translation
+from dashboard.forms import BaseEnrollmentFormSet, EnrollmentForm
 from django.contrib.auth.models import User
 from dashboard.models import Kompetisi, Peserta
 from django.contrib.auth.decorators import login_required
+from django.forms.formsets import formset_factory
 from django.http.response import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 # Create your views here.
 @login_required
@@ -21,8 +25,32 @@ def home(request):
 def enroll(request, id_kompetisi):
     context = {}
 
-    context["competition"] = Kompetisi.objects.get(id=id_kompetisi)
+    EnrollmentFormSet = formset_factory(EnrollmentForm, formset=BaseEnrollmentFormSet)
 
+    if request.method == "POST":
+        enrollment_formset = EnrollmentFormSet(request.POST)
+
+        if enrollment_formset.is_valid():
+            kompetisi = Kompetisi.objects.get(id=id_kompetisi)
+
+            peserta_baru = []
+
+            for enrollment_form in enrollment_formset:
+                nama = enrollment_form.cleaned_data["nama"]
+                npm = enrollment_form.cleaned_data["npm"]
+
+                if nama and npm:
+                    peserta_baru.append(Peserta(nama=nama, npm=npm, kompetisi=kompetisi))
+            
+            Peserta.objects.bulk_create(peserta_baru)
+
+            return redirect(reverse("dashboard:home"))
+        
+        else:
+            context["enrollment_forms"] = enrollment_formset
+            return render(request, "dashboard/enroll.html", context)
+
+    context["enrollment_forms"] = EnrollmentFormSet()
     return render(request, "dashboard/enroll.html", context)
 
 
