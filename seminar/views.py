71366d7a26fbc9c3ee661c242a1e7e81395d4856
ddelvo <ariasenacahya@gmail.com>
2021-08-05 16:40:12 +0700

    Added carousell

--- seminar/views.py
+++ seminar/views.py
@@ -2,13 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Seminar
 from django.utils import timezone
+import random
 
 
 # Create your views here.
 
-def seminarlanding(request):
+def getairtime(dt_data):
+    air_time = str(dt_data.days) + "days" + " " + str(dt_data.seconds // 3600) + "hrs" + " " + str(
+        (dt_data.seconds // 60) % 60) + "mins"
+    return air_time
 
+def seminarlanding(request):
     semua_seminar = Seminar.objects.all()
+
+    id_list = []
+    random_seminar = []
+    air_time = []
+    for seminar in semua_seminar:
+        id_list.append(seminar.id)
+
+    randomized_seminar = random.sample(id_list, 3)
+
+    for i in randomized_seminar:
+        # print(type(i))
+        temp_seminar = Seminar.objects.get(id=i)
+        air_time.append(temp_seminar.d_day - timezone.localtime())
+
+        if temp_seminar.d_day < timezone.localtime():
+            temp_seminar.is_past = True
+
+        random_seminar.append(temp_seminar)
+
     for seminar in semua_seminar:
 
         # Find latest event from a list of events from a seminar
@@ -17,16 +41,23 @@ def seminarlanding(request):
         if seminar.d_day < timezone.localtime():
             seminar.is_past = True
 
+    print(type(random_seminar[0]))
 
     argument = {
-        'seminar_list': semua_seminar
+        'seminar_list': semua_seminar,
+        'first_seminar': random_seminar[0],
+        'second_seminar': random_seminar[1],
+        'third_seminar': random_seminar[2],
+        'first_air_time': getairtime(air_time[0]),
+        'second_air_time': getairtime(air_time[1]),
+        'third_air_time': getairtime(air_time[2]),
     }
 
     return render(request, 'seminar_landing.html', argument)
 
 
 def getseminar(request, id_seminar):
-    seminar = Seminar.objects.get(id = id_seminar)
+    seminar = Seminar.objects.get(id=id_seminar)
     guest_stars = seminar.guest_stars.all()
     all_subjects = [x for x in seminar.subjects.split(';') if x]
 
@@ -34,7 +65,7 @@ def getseminar(request, id_seminar):
         seminar.is_past = True
 
     dt = seminar.d_day - timezone.localtime()
-    air_time = str(dt.days) + "days" + " " + str(dt.seconds//3600) + "hrs" + " " + str((dt.seconds//60)%60) + "mins"
+    air_time = getairtime(dt)
 
     argument = {
         'seminar': seminar,
@@ -44,6 +75,6 @@ def getseminar(request, id_seminar):
     }
     return render(request, 'each_seminar.html', argument)
 
+
 def getseminarform(request):
     return render(request, 'form_template.html')
-
