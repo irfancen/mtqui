a50ecf865298c227eee7d81904e856ba8039980e
Rafi Dirgantara <rafi.dirgan@gmail.com>
2021-08-05 17:39:20 +0700

    Add view for template

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def gallery(request):
-    return HttpResponse("Hello this is gallery page")
+    return render(request, 'gallery.html')
