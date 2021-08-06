2730e7b307401d7d38420e631e40ccc6575e5c74
Rafi Dirgantara <rafi.dirgan@gmail.com>
2021-08-06 13:52:38 +0700

    Add models for gallery

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Kegiatan(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class GalleryImage(models.Model):
+    name = models.CharField(max_length=255)
+    kegiatan = models.ForeignKey(Kegiatan, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
