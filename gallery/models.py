c6d9306961a6a1869154ce26035a1775e7506b4d
Rafi Dirgantara <rafi.dirgan@gmail.com>
2021-08-13 17:58:45 +0700

    Add ImageField for gallery app

--- gallery/models.py
+++ gallery/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
+
 class Kegiatan(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
+
 class GalleryImage(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True)
     kegiatan = models.ForeignKey(Kegiatan, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="gallery", blank=True)
 
     def __str__(self):
-        return self.name
+        return self.image.name
