d6141b36c930d2796af92008896e0205459ada42
Rafi Dirgantara <rafi.dirgan@gmail.com>
2021-08-13 17:59:31 +0700

    Create view for images

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Kegiatan, GalleryImage
 
 
 def gallery(request):
-    return render(request, 'gallery.html')
+    grand_opening_img = GalleryImage.objects.filter(kegiatan__name="Grand Opening")
+    competition_img = GalleryImage.objects.filter(kegiatan__name="Competition")
+    closing_ceremony_img = GalleryImage.objects.filter(kegiatan__name="Closing Ceremony")
+
+    context = {
+        'go_img': grand_opening_img,
+        'comp_img': competition_img,
+        'cc_img': closing_ceremony_img,
+    }
+
+    return render(request, 'gallery.html', context)
