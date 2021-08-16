5dca68f40145cb18fc5cc7191d3d4a4f8dc1682d
Gabriel Enrique <gabrielenrique.abel@gmail.com>
2021-08-17 01:52:21 +0700

    Setup urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,7 +6,14 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', home, name='home'),
     path('enroll/<int:id_kompetisi>', enroll, name='enroll'),
-    path('edit/<int:id_kompetisi>', edit_enrollments, name='edit'),
-    path('view/<int:id_kompetisi>', view_enrollments, name='view'),
+    path('add/<int:id_kelompok>', add_anggota, name='add_anggota'),
+    path('edit/participant/<int:id_peserta>', edit_peserta, name='edit_peserta'),
+    path('edit/team/<int:id_kelompok>', edit_kelompok, name='edit_kelompok'),
+    path('edit/member/<int:id_anggota>', edit_anggota, name='edit_anggota'),
+    path('delete/participant/<int:id_peserta>', delete_peserta, name='delete_peserta'),
+    path('delete/team/<int:id_kelompok>', delete_kelompok, name='delete_kelompok'),
+    path('delete/member/<int:id_anggota>', delete_anggota, name='delete_anggota'),
+    path('view/<int:id_kompetisi>', view_enrollments, name='view_enrollments'),
+    path('view/team/<int:id_kelompok>', view_kelompok, name='view_kelompok'),
     path("debug/", debug, name="debug"),
 ]
