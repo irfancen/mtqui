83c31851226d225f6f930fa2a61d6eadf297b547
Gabriel Enrique <gabrielenrique.abel@gmail.com>
2021-08-19 23:19:41 +0700

    Change formset to single forms

--- dashboard/views.py
+++ dashboard/views.py
@@ -83,55 +83,50 @@ def enroll_kelompok(request, id_kompetisi):
     context = {}
 
     kompetisi = Kompetisi.objects.get(id=id_kompetisi)
-    AnggotaFormSet = formset_factory(AnggotaForm, max_num=kompetisi.kapasitas_kelompok, min_num=1, extra=0)
 
     if request.method == "POST":
         kelompok_form = KelompokForm(request.POST)
-        anggota_formset = AnggotaFormSet(request.POST, request.FILES)
+        anggota_form = AnggotaForm(request.POST, request.FILES)
 
-        if kelompok_form.is_valid() and anggota_formset.is_valid():
+        if kelompok_form.is_valid() and anggota_form.is_valid():
             nama_kelompok = kelompok_form.cleaned_data.get("nama")
 
             kelompok = Kelompok(nama=nama_kelompok, kompetisi=kompetisi)
             kelompok.save()
 
-            anggota_list = []
+            nama = anggota_form.cleaned_data.get("nama")
+            fakultas = request.user.metadata.nama_fakultas
+            jurusan = anggota_form.cleaned_data.get("jurusan")
+            angkatan = anggota_form.cleaned_data.get("angkatan")
+            no_hp = anggota_form.cleaned_data.get("no_hp")
+            line_id = anggota_form.cleaned_data.get("line_id")
+            foto_ktm = anggota_form.cleaned_data.get("foto_ktm")
+            screenshot_siak = anggota_form.cleaned_data.get("screenshot_siak")
 
-            for anggota_form in anggota_formset:
-                nama = anggota_form.cleaned_data.get("nama")
-                fakultas = request.user.metadata.nama_fakultas
-                jurusan = anggota_form.cleaned_data.get("jurusan")
-                angkatan = anggota_form.cleaned_data.get("angkatan")
-                no_hp = anggota_form.cleaned_data.get("no_hp")
-                line_id = anggota_form.cleaned_data.get("line_id")
-                foto_ktm = anggota_form.cleaned_data.get("foto_ktm")
-                screenshot_siak = anggota_form.cleaned_data.get("screenshot_siak")
-
-                anggota_list.append(Anggota(
-                            nama=nama, 
-                            fakultas=fakultas, 
-                            jurusan=jurusan, 
-                            angkatan=angkatan, 
-                            no_hp=no_hp, 
-                            line_id=line_id, 
-                            foto_ktm=foto_ktm, 
-                            screenshot_siak=screenshot_siak, 
-                            kelompok=kelompok
-                        ))
-
-            Anggota.objects.bulk_create(anggota_list)
+            anggota = Anggota(
+                        nama=nama, 
+                        fakultas=fakultas, 
+                        jurusan=jurusan, 
+                        angkatan=angkatan, 
+                        no_hp=no_hp, 
+                        line_id=line_id, 
+                        foto_ktm=foto_ktm, 
+                        screenshot_siak=screenshot_siak, 
+                        kelompok=kelompok
+                    )
+            anggota.save()
 
             return redirect(reverse("dashboard:home"))
     
         else:
             context["competition"] = kompetisi
             context["kelompok_form"] = kelompok_form
-            context["anggota_formset"] = anggota_formset
+            context["anggota_form"] = anggota_form
             return render(request, "dashboard/enroll_kelompok.html", context)
     
     context["competition"] = kompetisi
     context["kelompok_form"] = KelompokForm()
-    context["anggota_formset"] = AnggotaFormSet()
+    context["anggota_form"] = AnggotaForm()
     return render(request, "dashboard/enroll_kelompok.html", context)
 
 
@@ -139,59 +134,54 @@ def enroll_daq(request, id_kompetisi):
     context = {}
 
     kompetisi = Kompetisi.objects.get(id=id_kompetisi)
-    AnggotaDAQFormSet = formset_factory(AnggotaDAQForm, formset=BaseAnggotaDAQFormSet, max_num=kompetisi.kapasitas_kelompok, min_num=1, extra=0)
 
     if request.method == "POST":
         kelompok_form = KelompokForm(request.POST)
-        anggota_daq_formset = AnggotaDAQFormSet(request.POST, request.FILES)
+        anggota_daq_form = AnggotaDAQForm(request.POST, request.FILES)
 
-        if kelompok_form.is_valid() and anggota_daq_formset.is_valid():
+        if kelompok_form.is_valid() and anggota_daq_form.is_valid():
             nama_kelompok = kelompok_form.cleaned_data.get("nama")
 
             kelompok = Kelompok(nama=nama_kelompok, kompetisi=kompetisi)
             kelompok.save()
 
-            anggota_daq_list = []
+            nama = anggota_daq_form.cleaned_data.get("nama")
+            fakultas = request.user.metadata.nama_fakultas
+            jurusan = anggota_daq_form.cleaned_data.get("jurusan")
+            angkatan = anggota_daq_form.cleaned_data.get("angkatan")
+            no_hp = anggota_daq_form.cleaned_data.get("no_hp")
+            line_id = anggota_daq_form.cleaned_data.get("line_id")
+            is_ketua = True
+            foto_ktm = anggota_daq_form.cleaned_data.get("foto_ktm")
+            screenshot_siak = anggota_daq_form.cleaned_data.get("screenshot_siak")
+            file_cv = anggota_daq_form.cleaned_data.get("file_cv")
 
-            for anggota_daq_form in anggota_daq_formset:
-                nama = anggota_daq_form.cleaned_data.get("nama")
-                fakultas = request.user.metadata.nama_fakultas
-                jurusan = anggota_daq_form.cleaned_data.get("jurusan")
-                angkatan = anggota_daq_form.cleaned_data.get("angkatan")
-                no_hp = anggota_daq_form.cleaned_data.get("no_hp")
-                line_id = anggota_daq_form.cleaned_data.get("line_id")
-                is_ketua = anggota_daq_form.cleaned_data.get("is_ketua")
-                foto_ktm = anggota_daq_form.cleaned_data.get("foto_ktm")
-                screenshot_siak = anggota_daq_form.cleaned_data.get("screenshot_siak")
-                file_cv = anggota_daq_form.cleaned_data.get("file_cv")
-
-                anggota_daq_list.append(Anggota(
-                            nama=nama,
-                            fakultas=fakultas,
-                            jurusan=jurusan,
-                            angkatan=angkatan,
-                            no_hp=no_hp,
-                            line_id=line_id,
-                            is_ketua=is_ketua,
-                            foto_ktm=foto_ktm,
-                            screenshot_siak=screenshot_siak,
-                            file_cv=file_cv,
-                            kelompok=kelompok
-                        ))
-            
-            Anggota.objects.bulk_create(anggota_daq_list)
+            anggota = Anggota(
+                        nama=nama,
+                        fakultas=fakultas,
+                        jurusan=jurusan,
+                        angkatan=angkatan,
+                        no_hp=no_hp,
+                        line_id=line_id,
+                        is_ketua=is_ketua,
+                        foto_ktm=foto_ktm,
+                        screenshot_siak=screenshot_siak,
+                        file_cv=file_cv,
+                        kelompok=kelompok
+                    )
+            anggota.save()
 
             return redirect(reverse("dashboard:home"))
 
         else:
             context["competition"] = kompetisi
             context["kelompok_form"] = kelompok_form
-            context["anggota_daq_formset"] = anggota_daq_formset
+            context["anggota_daq_form"] = anggota_daq_form
             return render(request, "dashboard/enroll_daq.html", context)
     
     context["competition"] = kompetisi
     context["kelompok_form"] = KelompokForm()
-    context["anggota_daq_formset"] = AnggotaDAQFormSet()
+    context["anggota_daq_form"] = AnggotaDAQForm()
     return render(request, "dashboard/enroll_daq.html", context)
 
 
