df5a8c01f124419894bfd9083fce49b0a1322f92
M. Irfan Junaidi <mirfanjunaidi@gmail.com>
2021-08-21 14:08:42 +0700

    Add Guidebook

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage, guidebook
 
 app_name = "homepage"
 
 urlpatterns = [
-    path('', homepage, name='homepage')
+    path('', homepage, name='homepage'),
+    path('guidebook/', guidebook, name='guidebook')
 ]
