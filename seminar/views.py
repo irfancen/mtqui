870a9d86744fe8ab1d35446f4ddb181b4f8fd219
ddelvo <ariasenacahya@gmail.com>
2021-08-22 21:34:04 +0700

    Added grandopening and closing page and changed time format

--- seminar/views.py
+++ seminar/views.py
@@ -6,10 +6,6 @@ import random
 
 # Create your views here.
 
-def getairtime(dt_data):
-    air_time = str(dt_data.days) + "days" + " " + str(dt_data.seconds // 3600) + "hrs" + " " + str(
-        (dt_data.seconds // 60) % 60) + "mins"
-    return air_time
 
 def seminarlanding(request):
     semua_seminar = Seminar.objects.all()
@@ -23,10 +19,8 @@ def seminarlanding(request):
     for seminar in semua_seminar:
         id_list.append(seminar.id)
 
-
-
     # Get 3 or less random seminar
-    if(Seminar.objects.count() < 3):
+    if (Seminar.objects.count() < 3):
         randomized_seminar = random.sample(id_list, Seminar.objects.count())
     else:
         randomized_seminar = random.sample(id_list, 3)
@@ -34,7 +28,6 @@ def seminarlanding(request):
     # For each carousel seminar, get its air time and check if past or not
     for i in randomized_seminar:
         temp_seminar = Seminar.objects.get(id=i)
-        temp_seminar.air_time = getairtime(temp_seminar.d_day - timezone.localtime())
 
         if temp_seminar.d_day < timezone.localtime():
             temp_seminar.is_past = True
@@ -70,8 +63,6 @@ def getseminar(request, id_seminar):
     if seminar.d_day < timezone.localtime():
         seminar.is_past = True
 
-    seminar.air_time = getairtime(seminar.d_day - timezone.localtime())
-
     argument = {
         'seminar': seminar,
         'guest_stars': guest_stars,
@@ -79,6 +70,8 @@ def getseminar(request, id_seminar):
         'all_biografi': all_biografi
     }
     return render(request, 'each_seminar.html', argument)
+
+
 
 
 def getseminarform(request):
